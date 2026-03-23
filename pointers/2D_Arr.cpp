bac3a2ca296b870205d2c821709289e6651aa233
Pansare Komal <161268248+Pansarekomal039@users.noreply.github.com>
2026-03-23 22:51:42 +0530

    Update 2D_Arr.cpp

--- pointers/2D_Arr.cpp
+++ pointers/2D_Arr.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int rows = 3;  int cols = 4;
-    int **arr = new int *[rows];
+    int **arr = new int *[rows]; 
     for(int i = 0; i < rows; i++)
     arr[i] = new int [cols];
 int val = 1;
